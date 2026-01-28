5d7b379e2f6c1378798d7d3b75dd2a345a8b90e9
g3rar2 <>
2026-01-28 15:02:21 -0600

    Restructura de folder login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: 'home',
-    loadComponent: () => import('./pages/home/home/home.page').then((m) => m.HomePage),
+    loadComponent: () => import('./pages/rides/home/home.page').then((m) => m.HomePage),
   },
   {
     path: '',
