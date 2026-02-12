29241bd9da82f72f14f235105010c47c56e8c384
g3rar2 <>
2026-02-12 17:23:44 -0600

    API integration

--- src/app/pages/rides/home/home.page.ts
+++ src/app/pages/rides/home/home.page.ts
@@ -1,18 +1,44 @@
-import {Component, inject, signal, WritableSignal} from '@angular/core';
-import {IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonToast} from '@ionic/angular/standalone';
+import {Component, inject, OnInit, signal, WritableSignal} from '@angular/core';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonButton,
+  IonToast,
+  IonItem,
+  IonLabel
+} from '@ionic/angular/standalone';
 import {ToastService} from "../../../services/shared/toast-service";
+import {PhotoApiService} from "../../../services/photos/photo-api-service";
+import {PhotoDto} from "../../../dtos/photos/photo-dto";
+import {AlertService} from "../../../services/shared/alert-service";
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonToast],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonToast, IonItem, IonLabel],
 })
-export class HomePage {
+export class HomePage implements OnInit {
   constructor() {}
 
+  private readonly _photoAPiService:PhotoApiService=inject(PhotoApiService)
+  private readonly _alertService:AlertService=inject(AlertService)
   private readonly _toastService: ToastService=inject(ToastService);
   showToast:WritableSignal<boolean>=signal(false);
+  photos:WritableSignal<PhotoDto[]>=this._photoAPiService.photos;
+  photo:WritableSignal<PhotoDto|null>=this._photoAPiService.photo;
+
+
+  newRideAlertButtons=[
+    {
+      text:'ok',
+      role:'confirm',
+      handler: () => {}
+    }
+  ]
+
 
   openToast():void{
     this.showToast.set(true);
@@ -30,5 +56,42 @@ export class HomePage {
     await this._toastService.showToast('Bienvenido a ionic');
     await this._toastService.showToast('A ocurrido un error', true);
   }
+
+  ngOnInit() {
+    this._photoAPiService.getAllPhotos()
+  }
+
+  async getPhotoByID(id:number):Promise<void>{
+    this._photoAPiService.getPhotoById(id)
+
+    const value=this.photo();
+
+    if (value) {
+      await this._alertService.showAlert({
+        header:`Nuevo viaje a ${value.title}`,
+        subHeader:'Deseas aceptar el nuevo viaje',
+        message:'Destino: Mall Galerias\nDiatancia" 5m\nTarifa: 200',
+        buttons:this.newRideAlertButtons
+      })
+    }
+
+  }
+
+
+  createPhoto():void{
+    const newPhoto:PhotoDto={
+      id:0,
+      title:'Nueva Foto',
+      url:'',
+      thumbnailUrl:'',
+      album:1,
+    }
+    this._photoAPiService.createPhoto(newPhoto)
+  }
+
+  deletePhoto(id:number):void{
+    this._photoAPiService.showAlertWhenDeletePhoto(id)
+  }
+
 
 }
--- src/app/services/photos/photo-api-service.ts
+++ src/app/services/photos/photo-api-service.ts
@@ -1,5 +1,10 @@
-import { Injectable } from '@angular/core';
+import {inject, Injectable, signal, WritableSignal} from '@angular/core';
 import {environment} from "../../../environments/environment";
+import {HttpClient} from "@angular/common/http";
+import {ToastService} from "../shared/toast-service";
+import {PhotoDto} from "../../dtos/photos/photo-dto";
+import {AlertService} from "../shared/alert-service";
+import {LoadingService} from "../shared/loading-service";
 
 const API_URL = `${environment.API_URL}photos`;
 
@@ -7,5 +12,174 @@ const API_URL = `${environment.API_URL}photos`;
   providedIn: 'root',
 })
 export class PhotoApiService {
+
+  private readonly _httpClient=inject(HttpClient);
+  private readonly _toastService=inject(ToastService);
+  private readonly _alertsService=inject(AlertService);
+  private readonly _loadingService=inject(LoadingService);
+
+  photos:WritableSignal<PhotoDto[]>=signal<PhotoDto[]>([]);
+  photo:WritableSignal<PhotoDto|null>=signal<PhotoDto|null>(null);
+
+
+  alerButtons=[
+    {
+      text:'Cancelar',
+      role:'destructive',
+      handler: (id:number) => {
+
+      }
+    }
+  ]
+
+
+ async createPhoto(photo:PhotoDto):Promise<void>{
+    await this._loadingService.createLoading('Creando Foto')
+    this._httpClient.post<PhotoDto>(API_URL, photo).subscribe({
+      next:async (response:PhotoDto)=>{
+        if(response){
+          this.photos.update((currentPhotos)=>[...currentPhotos,response]);
+          await this._loadingService.closeLoading();
+          await this._toastService.showToast('Foto creada con exito')
+        }
+      },
+      error: async (error:Error) => {
+        await this._loadingService.closeLoading();
+        await this._toastService.showToast(
+          'Ha ocurrido un error en el photo',
+          true,
+        )
+      }
+    })
+  }
+
+
+  async updatePhoto(photo:PhotoDto):Promise<void>{
+    await this._loadingService.createLoading('Actualizando Foto'),
+    this._httpClient.put<PhotoDto>(`${API_URL}/${photo.id}`,photo).subscribe({
+      next:async (response:PhotoDto)=>{
+        if(response){
+          await this._loadingService.closeLoading();
+          this.photo.update((currentPhotos)=>{
+            const index=currentPhotos.findIndex((p)=>p.id===response.id);
+            if (index !== -1){
+              const updatePhotos=[...currentPhotos];
+              updatePhotos[index]=response;
+              return updatePhotos
+            }
+            return currentPhotos
+          })
+          await this._toastService.showToast('Foto creada con exito')
+        }
+      },
+      error: async (error:Error) => {
+        await this._loadingService.closeLoading();
+        await this._toastService.showToast(
+          'Ha ocurrido un error en el photo',
+          true,
+        )
+      }
+    })
+  }
+
+
+
+
+ newRiodeAlertButtons=[
+    {
+      text:'ok',
+      role:'confirm',
+      handler: (id:number) => {
+
+      }
+    }
+  ]
+
+
+
+  async getAllPhotos():Promise<void>{
+     await this._loadingService.createLoading('Obteniendo fotos...')
+
+    this._httpClient.get<PhotoDto[]>(API_URL).subscribe({
+      next:(response:PhotoDto[])  => {
+        console.log(response);
+        this.photos.set(response);
+        this._loadingService.closeLoading()
+
+      },
+      error: async(error) => {
+        console.error('Ha ocurrido un error:', error);
+        await this._toastService.showToast(
+          'Ha Ocurrido un error al obtener las fotos',
+          true,
+        )
+      }
+    })
+}
+
+
+
+  getPhotoById(id: number): void {
+    this._httpClient.get<PhotoDto>(`${API_URL}/${id}`).subscribe({
+      next: (response: PhotoDto) => {
+        console.log(response);
+        this.photo.set(response); // Assuming this.photo is a WritableSignal
+      },
+      error: async (error) => {
+
+        console.error('Ha ocurrido un error:', error);
+
+        await this._toastService.showToast(
+          'Ha ocurrido un error al obtener la foto',
+          true
+        );
+      }
+    });
+  }
+
+
+  deletePhotoById(id: number): void {
+    this._httpClient.delete(`${API_URL}/${id}`).subscribe({
+      next:async ()  => {
+        await this._toastService.showToast('Foto eliminada con exito')
+        this.photos.update((currentPhotos)=>currentPhotos.filter(p=>p.id!==id))
+      },
+      error:async(error) => {
+        await this._toastService.showToast(
+          'Ha ocurrido un error al obtener la foto',
+          true,
+        )
+      }
+    })
+  }
+
+
+
+  async showAlertWhenDeletePhoto(id:number):Promise<void>{
+
+    await this._alertsService.showAlert({
+      header:'Cancelacion de viaje',
+      subHeader: 'Estas segurio de que deseas cancelar el viaje',
+      message:'Esta accion no se puede revertir',
+      buttons:[
+        {
+          text:'Cancelar',
+          role:'destructive',
+          handler:()=>{
+            console.log('Viaje Cancelado');
+          }
+        },
+        {
+          text:'Confirmar',
+          role:'cancel',
+          handler:()=>{
+            this.deletePhotoById(id)
+          }
+        }
+      ]
+    })
+
+
+  }
 
 }
